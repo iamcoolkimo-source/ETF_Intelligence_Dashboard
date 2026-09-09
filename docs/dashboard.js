d772e763ace2787bb5bdd955ae67620fc4a74b92
iamcoolkimo-source <iamcoolkimo@gmail.com>
2026-09-09 12:37:19 +0900

    Update dashboard.js

--- docs/dashboard.js
+++ docs/dashboard.js
@@ -1,3 +1,50 @@
-console.log(
-    "ETF Dashboard Loaded"
-);
+
+async function loadETFList() {
+
+    const response = await fetch(
+        "./data/ALL_ETF_MASTER_V2.csv"
+    );
+
+    const text = await response.text();
+
+    const rows = text.split("\n");
+
+    const tbody =
+        document.querySelector(
+            "#etf-table tbody"
+        );
+
+    tbody.innerHTML = "";
+
+    rows.slice(1).forEach(row => {
+
+        if (!row.trim())
+            return;
+
+        const cols = row.split(",");
+
+        const tr =
+            document.createElement("tr");
+
+        tr.innerHTML = `
+            <td>
+                <input type="checkbox">
+            </td>
+
+            <td>${cols[0]}</td>
+
+            <td>${cols[1]}</td>
+
+            <td>${cols[2]}</td>
+
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+        `;
+
+        tbody.appendChild(tr);
+
+    });
+}
+
+loadETFList();
